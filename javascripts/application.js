2fd40ced470f1839da9af753bb1e4880d14785b4
Daniel Wearne <wernah@cultureamp.com>
2016-05-30 15:46:34 +1000

    Automated commit at 2016-05-30 05:46:34 UTC by middleman-deploy 1.0.0

--- javascripts/application.js
+++ javascripts/application.js
@@ -7,4 +7,18 @@ $(function() {
     e.preventDefault()
     $(this).parent().parent().removeClass("open");
   });
+
+
+
+  var header = $(".top_section");
+  $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+
+      if (scroll >= 85) {
+          header.addClass("scrolled_past");
+      } else {
+          header.removeClass("scrolled_past");
+      }
+  });
+
 });
